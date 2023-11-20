91a10e52198371d9c190fb0785fd44e13179215e
vitin <victorgaspalmeida@hotmail.com>
2023-11-20 13:12:15 -0300

    5 de arquivos

---
+++ Algoritmos/Lista 8/ex01.c
@@ -0,0 +1,19 @@
+/*1. Escreva uma função que verifica se o arquivo de caminho informado existe (retorna 1). Caso
+contrário, retorna 0.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int fileExists(const char *filepath){
+    FILE *file = fopen(filepath, "r");
+    if(!file){
+        return 0;
+    }
+    return 1;
+}
+
+int main(){
+    int res = fileExists("./biscoito.txt");
+    printf("%d\n", res);
+}
---
+++ Algoritmos/Lista 8/ex02.c
@@ -0,0 +1,21 @@
+/*2. Escreva uma função que receba um caminho de arquivo. Se o mesmo existir, faz nada. Se o
+arquivo não existir, tenta criá-lo. A função deve retornar 1 (um) caso arquivo exista ou tenha sido
+criado e, 0 (zero), caso contrário.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int checkOrCreate(const char *filepath){
+    FILE *file = fopen(filepath, "r");
+    if(!file){
+        file = fopen(filepath, "w");
+        if(!file)
+            return 0;
+    }
+    return 1;
+}
+
+int main(){
+    int res = checkOrCreate("./biscoito.txt");
+    printf("%d\n", res);
+}
